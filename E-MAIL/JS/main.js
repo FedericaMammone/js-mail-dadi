c3b6c376873c275449cf5a37d7a1362bce3455bc
Fef97 <federicakr97@gmail.com>
2023-05-03 15:36:14 +0200

    aggiunti commenti

--- E-MAIL/JS/main.js
+++ E-MAIL/JS/main.js
@@ -1,1 +1,14 @@
 const mail = ['fede@mail.com', 'ciccio@panzo.com', 'anita@hey.it', 'buli@boolean.it', 'alex@karev.com', 'derek@shep.com' ];
+
+// selezioniamo il bottone
+
+// agganciamo l'evento al bottone
+
+
+// prendi il valore dell'input
+
+// creiamo condizione
+// se la mail inserita è presente nell'array, fornire messaggio conferma invito
+
+// altrimenti
+// fornire messaggio nagato invito
