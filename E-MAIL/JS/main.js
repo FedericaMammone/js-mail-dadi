938e1b59017e85fd662fdb0b221069661895c66f
Fef97 <federicakr97@gmail.com>
2023-05-03 20:59:21 +0200

    esercizio completato

--- E-MAIL/JS/main.js
+++ E-MAIL/JS/main.js
@@ -1,14 +1,31 @@
 const mail = ['fede@mail.com', 'ciccio@panzo.com', 'anita@hey.it', 'buli@boolean.it', 'alex@karev.com', 'derek@shep.com' ];
 
-// selezioniamo il bottone
+let messaggio = "la tua mail NON è valida";
+// console.log(messaggio);
 
-// agganciamo l'evento al bottone
+let container = document.getElementById('container');
+
+let mailUser = prompt("Inserisci la tua mail");
+// console.log(mailUser);
 
 
-// prendi il valore dell'input
+// traversiamo l'array
+for (let i = mail.length - 1; i >= 0; i--) {
+    
+    const itemMail = mail[i]
+    // console.log(itemMail);
 
-// creiamo condizione
-// se la mail inserita è presente nell'array, fornire messaggio conferma invito
+    // creiamo condizione
+    // se la mail inserita è presente nell'array, fornire messaggio conferma invito
+    if (mailUser === itemMail) {
+        messaggio = "la tua mail è valida";
+        
+    }
+    
+    
+    
+}
 
-// altrimenti
-// fornire messaggio nagato invito
+const benvenutoMex =document.createElement('h1');
+    benvenutoMex.append(messaggio);
+    container.append(benvenutoMex);
