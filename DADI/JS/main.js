082e0ec0a21ae86baa01f9b5a67c983c0b77f36a
Fef97 <federicakr97@gmail.com>
2023-05-04 17:26:31 +0200

    Inseriti messaggi con numeri generati nell'output

--- DADI/JS/main.js
+++ DADI/JS/main.js
@@ -1,6 +1,9 @@
 // variabili
-const bottone = document.getElementById("push")
-let numeroUser, numeroPC
+const bottone = document.getElementById("push");
+let numeroUser, numeroPC;
+
+let messaggioUser = "Il numero generato per l'utente è = "; 
+let messaggioPC = "Il numero generato per il pc è = ";
 
 // collego evento al bottone
 bottone.addEventListener('click', miaFunzione);
@@ -16,11 +19,13 @@ function miaFunzione() {
     numeroPC = Math.floor(Math.random() * 6) + 1;
     console.log(numeroPC);
 
-    // mostro in output  i due numeri generati conil messaggio
-
+    // mostro in output  i due numeri generati con il messaggio
+    document.getElementById('userNumber').innerHTML = messaggioUser + numeroUser;
+    document.getElementById('pcNumber').innerHTML = messaggioPC + numeroPC;
+    
     // creo condizione
 
-
+    
 }
 
 
