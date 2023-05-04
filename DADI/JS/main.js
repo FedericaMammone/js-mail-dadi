1d4b43b5d09e8e37d7f35a768ad400e4d4f52121
Fef97 <federicakr97@gmail.com>
2023-05-04 17:13:02 +0200

    aggiunte variabili e commenti

---
+++ DADI/JS/main.js
@@ -0,0 +1,17 @@
+// variabili
+const bottone = document.getElementById("push")
+let numeroUser, numeroPC
+
+// collego evento al bottone
+
+// collego evento alla funzione
+
+// genero numero utente
+
+// genero numero pc
+
+// mostro in output  i due numeri generati conil messaggio
+
+// creo condizione
+
+
